f484097580430d2893a7abae7ba4d0181d78174c
Ailten <faouzi26b@live.fr>
2026-04-09 12:05:18 +0200

    spell add

---
+++ classPy/Spell.py
@@ -0,0 +1,13 @@
+
+class Spell():
+    def __init__(self, value: int, element: int):
+        self.value = value
+        self.element = element
+        self.isCrit = False
+        self.isParade = False
+
+    def clone(self) -> "Spell":
+        instance = Spell(self.value, self.element)
+        instance.isCrit = self.isCrit
+        instance.isParade = self.isParade
+        return instance
