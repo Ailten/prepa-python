45a1d501de3e3a6363fccbcdd30c2c47af29cff7
Ailten <faouzi26b@live.fr>
2026-04-17 10:34:50 +0200

    slime

---
+++ classPy/Monster/Slime.py
@@ -0,0 +1,49 @@
+from classPy.LivingEntity import LivingEntity
+
+class Slime(LivingEntity):
+    def __init__(self):
+        super().__init__('Slime')
+        self.lvl = 2
+        self.spells = {
+            'gougoute': Spell.initDetails(
+                value=1,
+                rangValue=6,
+                element=int(Elements.WATER),
+                name='gougoute'
+            ),
+            'visquausité': Spell.initDetails(
+                value=20,
+                element=int(Elements.WATER),
+                name='visquausité',
+                cooldown=3
+            ),
+            'englué': Spell.initDetails(
+                value=3,
+                element=int(Elements.WATER),
+                name='englué',
+                cooldown=4
+            )
+        }
+
+    def playTurn(self, oponent) -> str:
+
+        # debuff.
+        if(self.spells['visquausité'].turnUntilReDo == 1):
+            self.dmg[self.spells['visquausité'].element] -= self.spells['visquausité'].value
+            print(f'{self.name} perd visquausité.')
+        if(self.spells['englué'].turnUntilReDo == 1):
+            pass
+        
+        # pick spell.
+        if(self.spells['englué'].isCanBePlay(self.lvl) and self.HP.getPurcent <= 0.3):
+            self.spells['englué'].resetCooldown()
+            oponent.res[self.spells['englué'].element] -= self.spells['englué'].value / 100  # TODO: make a function for effects malus (obligator).
+            elemStr = Elements(self.spells['englué'].element).getName()
+            return f'{self.name} lance englué ! ({self.spells['englué'].value} {elemStr})'
+        elif(self.spells['visquausité'].isCanBePlay(self.lvl) and self.HP.getPurcent >= 0.6):
+            self.spells['visquausité'].resetCooldown()
+            return self.selfBoostDamage(self.spells['visquausité'].clone())
+        return self.atkOponent(oponent, self.spells['gougoute'].clone())
+
+
+
---
+++ classPy/Monster/Souchon.py
@@ -0,0 +1,47 @@
+from classPy.LivingEntity import LivingEntity
+
+class Souchon(LivingEntity):
+    def __init__(self):
+        super().__init__('Souchon')
+        self.lvl = 2
+        self.spells = {
+            'fouet racine': Spell.initDetails(
+                value=6,
+                rangValue=4,
+                element=int(Elements.EARTH),
+                name='fouet racine'
+            ),
+            'drain': Spell.initDetails(
+                value=10,
+                rangValue=2,
+                isSteelHP=True,
+                element=int(Elements.WATER),
+                name='drain',
+                cooldown=3
+            ),
+            'bourgeon neuf': Spell.initDetails(
+                value=2,
+                element=int(Elements.EARTH),
+                name='bourgeon neuf',
+                cooldown=6
+            )
+        }
+
+    def playTurn(self, oponent) -> str:
+
+        # debuff.
+        if(self.spells['bourgeon neuf'].turnUntilReDo == 2):
+            self.dmg[self.spells['bourgeon neuf'].element] -= self.spells['bourgeon neuf'].value
+            print(f'{self.name} perd bourgeon neuf.')
+        
+        # pick spell.
+        if(self.spells['drain'].isCanBePlay(self.lvl) and self.HP.getPurcent <= 0.5):
+            self.spells['drain'].resetCooldown()
+            return self.selfBoostResAllElements(self.spells['drain'].clone())
+        elif(self.spells['bourgeon neuf'].isCanBePlay(self.lvl) and self.HP.getPurcent >= 0.9):
+            self.spells['bourgeon neuf'].resetCooldown()
+            return self.selfBoostDamage(self.spells['bourgeon neuf'].clone())
+        return self.atkOponent(oponent, self.spells['fouet racine'].clone())
+
+
+
