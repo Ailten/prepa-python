89fe565b84cb91a41fdb2ac682ae4b3592a997df
Ailten <faouzi26b@live.fr>
2026-04-18 10:38:11 +0200

    adventurer manager

---
+++ classPy/Adventurer/AdventurerManager.py
@@ -0,0 +1,20 @@
+from classPy.Mercenaire import Mercenaire
+from classPy.Healer import Healer
+from classPy.Tank import Tank
+
+class AdventurerManager():
+
+    @classmethod
+    def getAnAdventurer(nameClassAdventurer: str):
+        match nameClassAdventurer:
+            case 'Mercenaire':
+                return Mercenaire
+            case 'Healer':
+                return Healer
+            case 'Tank':
+                return Tank
+            case _:
+                raise Exception('no class match')
+
+    
+
